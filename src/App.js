4441ee25279bcfef96592fd634f145a08aae3d1c
lordinit <l1zzard2444@gmail.com>
2023-08-24 16:34:01 +0300

    atom

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const logoCount = 1000; // Количество повторений логотипа
+  const logos = Array.from({ length: logoCount }, (_, index) => (
+    <img key={index} src={logo} className="App-logo" alt="logo" />
+  ));
+
   return (
     <div className="App">
       <header className="App-header">
         <div className="logos-container">
-          <img src={logo} className="App-logo" alt="logo" />
-          <img src={logo} className="App-logo" alt="logo" />
-          <img src={logo} className="App-logo" alt="logo" />
-          <img src={logo} className="App-logo" alt="logo" />
+          {logos}
         </div>
       </header>
     </div>
