1bc36b11c16a50a17e433b1dbc619ed449d46fa2
lordinit <l1zzard2444@gmail.com>
2023-08-30 10:16:20 +0300

    com

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,14 +3,17 @@ import { AppModule } from './app.module';
 import * as fs from 'fs';
 
 async function bootstrap() {
-  const domain = '1562863-cd94457.twc1.net';
-  const httpsOptions = {
-    key: fs.readFileSync(`../../nginx/certs/${domain}/key.pem`),
-    cert: fs.readFileSync(`../../nginx/certs/${domain}/cert.pem`),
-  };
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions,
-  });
+  // const domain = '1562863-cd94457.twc1.net';
+  // const httpsOptions = {
+  //   key: fs.readFileSync(`../../nginx/certs/${domain}/key.pem`),
+  //   cert: fs.readFileSync(`../../nginx/certs/${domain}/cert.pem`),
+  // };
+  const app = await NestFactory.create(
+    AppModule,
+    //{
+    //   httpsOptions,
+    // }
+  );
   await app.listen(8080);
 }
 bootstrap();
