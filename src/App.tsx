91349d498c12c82c6048ddeb9e1d8619daff6735
lordinit <l1zzard2444@gmail.com>
2023-09-12 22:45:36 +0300

    rotatespeed

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [logoCount, setLogoCount] = useState(4); // Используем хук useState для управления количеством логотипов
+  const [logoCount, setLogoCount] = useState(1); 
+
+  const maxSpeed = 100;
+  const [rotationSpeed, setRotationSpeed] = useState(1);
+
   const logos = Array.from({ length: logoCount }, (_, index) => (
     <img key={index} src={logo} className="App-logo" alt="logo" />
   ));
 
   const increaseLogoCount = () => {
-    setLogoCount(logoCount + 1); // Увеличиваем количество логотипов на 1
+    setLogoCount(logoCount + 1); 
   };
-
   const decreaseLogoCount = () => {
     if (logoCount > 0) {
-      setLogoCount(logoCount - 1); // Уменьшаем количество логотипов на 1 (если оно больше 0)
+      setLogoCount(logoCount - 1); 
     }
+  }
+  
+  useEffect(() => {
+    const logoElements = document.querySelectorAll('.App-logo');
+    logoElements.forEach((logoElement) => {
+      const htmlElement = logoElement as HTMLElement;
+      htmlElement.style.animationDuration  = `${rotationSpeed}s`;
+    });
+  }, [rotationSpeed, maxSpeed]);
+
+  const updateRotationSpeed = (value:number) => {
+    const newSpeed = maxSpeed - value;
+    setRotationSpeed(newSpeed);
   };
 
   return (
@@ -25,8 +41,20 @@ function App() {
           {logos}
         </div>
         <p>
-         ---- <code>Реконструкция</code> ----
+          ---- <code>Реконструкция</code> ----
         </p>
+        <div className="speed-control">
+          <label>Скорость вращения:</label>
+          <input
+            type="range"
+            min="0"
+            max={maxSpeed - 1}
+            step="1"
+            value={maxSpeed - rotationSpeed}
+            onChange={(e) => updateRotationSpeed(parseInt(e.target.value))}
+          />
+          <h1>{maxSpeed - rotationSpeed + 1}</h1>
+        </div>
         <div className="button-container">
           <button onClick={decreaseLogoCount}>-1</button>
           <button onClick={increaseLogoCount}>+1</button>
@@ -34,7 +62,7 @@ function App() {
         <a
           className="App-link"
           href="https://1562863-cd94457.twc1.net/api"
-          target='_blank'
+          target="_blank"
           rel="noopener noreferrer"
         >
           API
